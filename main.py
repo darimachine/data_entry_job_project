55fbe9db1ed93723e5d90f9e4bfcd4760d30ec41
Dari <serhi1334@gmail.com>
2022-07-03 03:44:52 +0300

    Webscraping Completed

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 FORMULQR_URL = "https://forms.gle/gNSEL1FM2qUDWpP66"
+from webscraping import ZillowScrap
+from pprint import pprint
+data = ZillowScrap()
+links = data.list_of_links()
+adresses = data.list_of_adresses()
+prices = data.list_of_prices()
+pprint(links)
+print(adresses)
+print(prices)
