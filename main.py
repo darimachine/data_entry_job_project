ccd1eaeea184ed81b698f6fa74634aa4a8ef5f0a
Dari <serhi1334@gmail.com>
2022-07-05 01:27:56 +0300

    Finished Variation

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-FORMULQR_URL = "https://forms.gle/gNSEL1FM2qUDWpP66"
+from Google import FillForm
 from webscraping import ZillowScrap
 from pprint import pprint
 data = ZillowScrap()
 links = data.list_of_links()
 adresses = data.list_of_adresses()
 prices = data.list_of_prices()
+google_fil = FillForm()
+google_fil.fill_form(links,adresses,prices)
 pprint(links)
 print(adresses)
 print(prices)
